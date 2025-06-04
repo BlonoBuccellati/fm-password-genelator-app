24e01f66d1721488a94a1caa43be1dcc02e47d66
BlonoBuccellati <119178322+BlonoBuccellati@users.noreply.github.com>
2025-06-05 03:50:36 +0900

    feat:準備

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
-import type { Metadata } from 'next';
-import '@/styles/globals.css';
+import type { Metadata } from "next";
+import { JetBrains_Mono } from "next/font/google";
+import "@/styles/globals.css";
 
 export const metadata: Metadata = {
-  title: 'Frontend Mentor | Password generator app',
-  description: 'Password generator app',
-  icons: 'favicon-32x32.png',
+  title: "Frontend Mentor | Password generator app",
+  description: "Password generator app",
+  icons: "favicon-32x32.png",
 };
+
+const jetBransMono = JetBrains_Mono({
+  variable: "--font-jet-brains-mono",
+  weight: ["700"],
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -13,8 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en'>
-      <body className={`antialiased`}>{children}</body>
+    <html lang="en">
+      <body className={`${jetBransMono.variable} antialiased`}>{children}</body>
     </html>
   );
 }
