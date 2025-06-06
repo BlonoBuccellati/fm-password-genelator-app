e2ca9cfd165dae94f434db4361d14a2007d5a63c
BlonoBuccellati <119178322+BlonoBuccellati@users.noreply.github.com>
2025-06-07 03:45:07 +0900

    style:レスポンシブ対応。slider箇所

--- src/features/password-generator/components/character-slider.tsx
+++ src/features/password-generator/components/character-slider.tsx
@@ -8,7 +8,7 @@ type SliderProps = React.ComponentProps<typeof Slider>;
 export function CharacterSlider({ className, ...props }: SliderProps) {
   const { length, handlerValueChange } = usePasswordGeneratorContext();
   return (
-    <div className={cn(className)}>
+    <div className={cn("space-y-clamp-100-to-200", className)}>
       <div className="flex items-center justify-between">
         <p className="typo-main text-grey-200">Character Length</p>
         <span className="typo-1 block text-green-200">{length}</span>
